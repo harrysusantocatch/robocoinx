d3b50e9afa2720b5ce3d58e962f5061c334f4eaa
Harry <bolobulet@gmail.com>
2019-10-04 17:39:22 +0700

    refactor

--- app/src/main/java/com/example/robocoinx/activity/SplashActivity.java
+++ app/src/main/java/com/example/robocoinx/activity/SplashActivity.java
@@ -19,6 +19,7 @@ import com.example.robocoinx.model.UserCache;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -27,6 +28,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -37,17 +40,81 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.splash_main);
         new Content().execute((Void)null);
 
-//        try {
-//            InputStream inputStream = getAssets().open("home.html");
-//            File file = new File("test");
-//            FileUtils.copy(inputStream, new FileOutputStream(file));
-//            Document document = Jsoup.parse(file, "UTF-8");
-//            Element elClientSeed = document.getElementById("next_client_seed");
-//            String clientSeed = elClientSeed.val();
-//            System.out.println(clientSeed);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            InputStream inputStream = getAssets().open("home.html");
+            Document document = Jsoup.parse(inputStream, "UTF-8", "");
+            String balance = getBalance(document);
+            String userID = "";
+            String rewardPoints = getRP(document);
+            int nextR = getNextR(document);
+            int btcBonusCountDown = getBTCBonusCountDown(document);
+            int rpBonusCountDown = getRPBonusCountDown(document);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getRPBonusCountDown(Document doc) {
+        String[] split = doc.html().split("BonusEndCountdown\\(\"fp_bonus\",");
+        if(split.length > 1){
+            Pattern pattern = Pattern.compile("[0-9]*.\\)");
+            Matcher matcher = pattern.matcher(split[1]);
+            if(matcher.find()){
+                return Integer.parseInt(matcher.group().substring(0, matcher.group().length()-1));
+            }
+        }
+        return 0;
+    }
+
+    private int getBTCBonusCountDown(Document doc) {
+        String[] split = doc.html().split("BonusEndCountdown\\(\"free_points\",");
+        if(split.length > 1){
+            Pattern pattern = Pattern.compile("[0-9]*.\\)");
+            Matcher matcher = pattern.matcher(split[1]);
+            if(matcher.find()){
+                return Integer.parseInt(matcher.group().substring(0, matcher.group().length()-1));
+            }
+        }
+        return 0;
+    }
+
+    private String getBalance(Document doc) {
+        String balance = null;
+        Element elBalance = doc.getElementById("balance");
+        balance = elBalance.text();
+        System.out.println("balance ="+balance);
+        return balance;
+    }
+
+    private String getRP(Document doc) {
+        String rp = null;
+        Elements elRP = doc.getElementsByClass("reward_table_box br_0_0_5_5 user_reward_points font_bold");
+        rp = elRP.text();
+        System.out.println("reward point ="+rp);
+        return rp;
+    }
+
+    private int getNextR(Document doc) {
+        String data = doc.html();
+        if(data.contains("free_play_time_remaining")){
+            String matcher = extractCountDown(data, "free_play_time_remaining");
+            if (matcher != null) return (Integer.parseInt(matcher));
+        }else if(data.contains("#time_remaining")){
+            String matcher = extractCountDown(data, "#time_remaining");
+            if (matcher != null) return (Integer.parseInt(matcher));
+        }
+        return 0;
+    }
+
+    private String extractCountDown(String data, String regex) {
+        String[] array = data.split(regex);
+        String input = array[1];
+        Pattern pattern = Pattern.compile("\\+.[0-9]*.\\,");
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            return matcher.group().substring(1, matcher.group().length()-1);
+        }
+        return null;
     }
 
     private void checkCsrfToken() {
